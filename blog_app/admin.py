fb932ee021acc69287bb38263b91e0e86f09c65e
Sulav Ghimire <sulavg2003@gmail.com>
2025-04-10 18:42:08 +0545

    updated admin

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Blog
+from .models import Blog, Like, Comment
 
 # Register your models here.
 
 admin.site.register(Blog)
+admin.site.register(Like)
+admin.site.register(Comment)
